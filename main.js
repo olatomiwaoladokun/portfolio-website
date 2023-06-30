7df32daa6007945fa13692c746c2049987ad8f1f
olatomiwaoladokun <olatomiwaoladokun@gmail.com>
2023-06-30 20:17:48 +0100

    Added style and function to the show more button

--- main.js
+++ main.js
@@ -8,10 +8,14 @@
 // restOfTheBodyButtton.addEventListener("click", openBody)
 
 const restOfTheBodyButton = document.getElementById("open-rest");
+const welcomePortion = document.querySelector(".welcome");
 
 function openbody (){
     const restOfThePage = document.querySelector(".others");
     restOfThePage.classList.add("others-clicked");
+    // Make the button and the welcome page disappear
+    welcomePortion.classList.add("welcome-diappear");
     restOfTheBodyButton.classList.add("open-rest-clicked");
+    restOfThePage.focus();
 }
 restOfTheBodyButton.addEventListener("click", openbody);
