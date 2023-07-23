5d768a87495d26c6e7a41e9762127293c135edf3
olatomiwaoladokun <olatomiwaoladokun@gmail.com>
2023-07-23 20:57:20 +0100

    Everything is almost working fine

--- main.js
+++ main.js
@@ -11,18 +11,11 @@ const restOfTheBodyButton = document.getElementById("open-rest");
 const welcomePortion = document.querySelector(".welcome");
 
 function openbody (){
-    const restOfThePage = document.querySelector(".others");
     restOfThePage.classList.add("others-clicked");
     // Make the button and the welcome page disappear
     welcomePortion.classList.add("welcome-disappear");
     restOfTheBodyButton.classList.add("open-rest-clicked");
-    restOfThePage.focus();
+    document
+    
 }
 restOfTheBodyButton.addEventListener("click", openbody);
-
-// Make the nav bar sticky
-
-windows.addEventListener("scroll", function(){
-    const navBar = document.querySelector(".navigation-tabs");
-    navBar.classList.toogle("sticky", window.scroolY > 0);
-});
