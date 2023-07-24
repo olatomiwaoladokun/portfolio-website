740dde771b9617059c7a21582d3c40ba7b4bfea3
olatomiwaoladokun <olatomiwaoladokun@gmail.com>
2023-07-24 22:10:19 +0100

    Ready to write cv in about page

--- main.js
+++ main.js
@@ -10,13 +10,12 @@
 const restOfTheBodyButton = document.getElementById("open-rest");
 const welcomePortion = document.querySelector(".welcome");
 const restOfThePage = document.querySelector(".others");
-
 function openbody (){
     restOfThePage.classList.add("others-clicked");
     // Make the button and the welcome page disappear
     welcomePortion.replaceWith ();
     // restOfTheBodyButton.classList.add("open-rest-clicked");
     
-}
+};
 
 restOfTheBodyButton.addEventListener("click", openbody);
