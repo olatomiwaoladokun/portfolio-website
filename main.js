22beb3a3685c38e11d74dc4dac2bcad8e5de9f20
olatomiwaoladokun <olatomiwaoladokun@gmail.com>
2023-07-24 16:02:46 +0100

    Did absolutely nothing

--- main.js
+++ main.js
@@ -9,13 +9,14 @@
 
 const restOfTheBodyButton = document.getElementById("open-rest");
 const welcomePortion = document.querySelector(".welcome");
+const restOfThePage = document.querySelector(".others");
 
 function openbody (){
     restOfThePage.classList.add("others-clicked");
     // Make the button and the welcome page disappear
-    welcomePortion.classList.add("welcome-disappear");
-    restOfTheBodyButton.classList.add("open-rest-clicked");
-    document
+    welcomePortion.replaceWith ();
+    // restOfTheBodyButton.classList.add("open-rest-clicked");
     
 }
+
 restOfTheBodyButton.addEventListener("click", openbody);
